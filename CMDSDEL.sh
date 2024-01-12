3f875d196694f29492d6a45a4e5f9b70cf5d3873
Holsen B. Moore <155948083+h0ls3n@users.noreply.github.com>
2024-01-11 21:07:22 -0600

    Update CMDSDEL.sh

--- CMDSDEL.sh
+++ CMDSDEL.sh
@@ -19,10 +19,7 @@ delen=0   # Used to record the final length of recursive_trinity.cmds in lines. 
 #### Step 0: Check if the correct files exist in the current directory. ####
 
 [ ! -f 'FailedCommands' ] && [ ! -f 'recursive_trinity.cmds' ] &&
-echo "
-The necessary files do not exist in this directory. Perhaps you are in the wrong directory or haven't run Trinity yet.
-:)
-" && exit 1
+echo "\nThe necessary files do not exist in this directory. Perhaps you are in the wrong directory or haven't run Trinity yet.\n:)" && exit 1
 
 #### Step 1: Create a backup of the recursive_trinity.cmds file in case we make a mistake, provided a backup doesn't already exist. ####
 
@@ -33,21 +30,15 @@ mkdir ../original_recursive_trinity.cmds && dirq=0
 cp recursive_trinity.cmds ../original_recursive_trinity.cmds && recq=0
 
 [ $recq -eq 0 ] &&
-echo '
-[Step 1]: Backup of recursive_trinity.cmds created in ../original_recursive_trinity.cmds.' ||
-echo '
-[Step 1]: Backup of recursive_trinity.cmds already exists in ../original_recursive_trinity.cmds.'
+echo -e '\n[Step 1]: Backup of recursive_trinity.cmds created in ../original_recursive_trinity.cmds.' ||
+echo -e '\n[Step 1]: Backup of recursive_trinity.cmds already exists in ../original_recursive_trinity.cmds.'
 
 #### Step 2: Gather the command IDs from the FailedCommands file. ####
 
 idar=( $(sed -n 's@\.trinity\.reads\.fa\.out.*@\\.@
                  s@.*/c@c@p' FailedCommands) )
-echo "
-[Step 2]: Gathered Command IDs from FailedCommands.
-The following ${#idar[@]} commands were gathered:
-
-${idar[@]%??}
-"
+echo -e "\n[Step 2]: Gathered Command IDs from FailedCommands.\nThe following ${#idar[@]} commands were gathered:\n
+${idar[@]%??}\n"
 
 #### Step 3: Delete failed commands from recursive_trinity.cmds following confirmation. ####
 
@@ -56,8 +47,7 @@ orlen=$(wc -l recursive_trinity.cmds)
 read -p "Do you want to delete these commands? (y/N): " del
 case $del in
     [yY])
-    echo "
-Deleting commands. Please wait..."
+    echo -e "\nDeleting commands. Please wait..."
     ;;
     *)
     echo "The program has been cancelled. Nothing has been deleted."
@@ -69,20 +59,13 @@ esac
 
 for n in ${idar[@]}
 do
-    sed -i "/$n/d" recursive_trinity.cmds
-    ((++tic))
+    (( ++tic ))
     per+="."
-    echo $(( 100 * $tic / ${#idar[@]} ))"% Done
-$per"
+    sed -i "/$n/d" recursive_trinity.cmds
+    echo -ne "    "$(( 100 * $tic / ${#idar[@]} ))"% Done $per\r"
 done
 
 delen=$(wc -l recursive_trinity.cmds)
 
-echo "
-[Step 3]: Deleted ${#idar[@]} failed commands from recursive_trinity.cmds.
-If all is well, you may now restart Trinity :)
-
-File Length (Lines)
-Before: $orlen
- After: $delen 
-"
+echo -ne "\n\n[Step 3]: Deleted ${#idar[@]} failed commands from recursive_trinity.cmds.\nIf all is well, you may now restart Trinity :)\n
+File Length (Lines)\nBefore: $orlen\n After: $delen\n"
