e3d4dcc420320006611b547ac80dd8781485a9a3
Holsen B. Moore <155948083+holsenmoore@users.noreply.github.com>
2024-03-09 18:57:02 -0600

    edited code slightly

--- csblast.py
+++ csblast.py
@@ -10,9 +10,17 @@ import regex as re
 
 # Read trinity_ids.txt and write to memory.
 with open("trinity_ids.txt","r") as file:
-    trinity_ids = file.readlines()
+    redundant_ids = file.readlines()
+
+# Remove duplicate IDs and output to terminal.
+purged_ids = list(set(redundant_ids))
+for i in purged_ids:
+    lone_id = re.sub(r'\n', "",i)
+    print(lone_id)
+
+print("")
 
 # Output AWK commands to terminal to search for read IDs.
-for i in trinity_ids:
+for i in purged_ids:
     lone_id = re.sub(r'\n',"",i)
     print("awk -F $'\\t' '/" + lone_id + "/ {print $1, $5}' ../script_map/*.sam")
--- csreads.py
+++ csreads.py
@@ -16,9 +16,11 @@ with open("raw_output.txt", "r") as file:
 purged_ids = list(set(redundant_ids))
 
 # Write new purged ID list to new file.
-with open("csreads-output.txt", "w") as file:
-    for i in purged_ids:
-        file.write(i)
+for i in purged_ids:
+    lone_id = re.sub(r'\n',"",i)
+    print(lone_id)
+
+print("")
 
 # Format the IDs into a new AWK command to search for transcript IDs.
 script_awk = []
